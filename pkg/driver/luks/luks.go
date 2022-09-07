0a100a6f0b96299bba067e8591e28f45ab95edfe
Maxime Dufour <maxime.dufour@outscale.com>
2022-09-07 15:49:42 +0000

    Add Luks encryption

---
+++ pkg/driver/luks/luks.go
@@ -0,0 +1,17 @@
+package luks
+
+type LuksContext struct {
+	Cipher  string
+	Hash    string
+	KeySize string
+}
+
+type LuksService interface {
+	IsLuks(devicePath string) bool
+	LuksFormat(devicePath string, passphrase string, context LuksContext) error
+	CheckLuksPassphrase(devicePath string, passphrase string) bool
+	LuksOpen(devicePath string, encryptedDeviceName string, passphrase string) (bool, error)
+	IsLuksMapping(devicePath string) (bool, string, error)
+	LuksResize(deviceName string) error
+	LuksClose(deviceName string) error
+}

0194131e8e4eb6cfdf418ffa228b3a90d4f2e45e
Maxime Dufour <maxime.dufour@outscale.com>
2023-02-15 14:54:13 +0000

    Upgrade to ginkgo v2

--- tests/e2e/testsuites/dynamically_provisioned_custom_pod_tester.go
+++ tests/e2e/testsuites/dynamically_provisioned_custom_pod_tester.go
@@ -18,7 +18,7 @@ import (
 	"fmt"
 	"regexp"
 
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	v1 "k8s.io/api/core/v1"
 	clientset "k8s.io/client-go/kubernetes"
 	"k8s.io/kubernetes/test/e2e/framework"

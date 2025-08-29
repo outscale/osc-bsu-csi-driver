d00b104a9e271063418823335120ab733568b5ac
Jean-François Bustarret <jean-francois.bustarret@outscale.com>
2025-08-29 11:40:17 +0000

    👷 ci: switch to new default branch, use oss image

--- .github/scripts/runneraksk.sh
+++ .github/scripts/runneraksk.sh
@@ -7,7 +7,3 @@ echo "OSC_SUBREGION_NAME=$az" | tee -a $GITHUB_ENV
 echo "OSC_REGION=$region" | tee -a $GITHUB_ENV
 echo "OSC_ACCESS_KEY_NAME=OSC_ACCESS_KEY_$suffix" | tee -a $GITHUB_ENV
 echo "OSC_SECRET_KEY_NAME=OSC_SECRET_KEY_$suffix" | tee -a $GITHUB_ENV
-echo "OMI_ID_NAME=OMI_ID_$suffix" | tee -a $GITHUB_ENV
-echo "IMG_UPGRADE_FROM_NAME=IMG_UPGRADE_FROM_$suffix" | tee -a $GITHUB_ENV
-echo "IMG_UPGRADE_TO_NAME=IMG_UPGRADE_TO_$suffix" | tee -a $GITHUB_ENV
-echo "IMG_MANAGEMENT_NAME=IMG_MANAGEMENT_$suffix" | tee -a $GITHUB_ENV
